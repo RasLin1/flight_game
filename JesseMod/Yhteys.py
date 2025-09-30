c5f7890e0bd1dfc0fb92701345c55715b63165bb
Jesseal12 <148433549+Jesseal12@users.noreply.github.com>
2025-09-30 19:54:53 +0300

    Pitäisi olla toimiva yhteys skriptti. Tein kaikille muos omat kansiot niiden skripteille

---
+++ JesseMod/Yhteys.py
@@ -0,0 +1,17 @@
+import mysql.connector
+
+yhteys = mysql.connector.connect (
+    host = "127.0.0.1",
+    port = 3306,
+    database="monster_game",
+    user = "monster_game",
+    password = "teamseven",
+    autocommit = True
+
+
+
+
+)
+
+
+
