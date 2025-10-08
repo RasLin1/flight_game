dc5659030cad253292b24e2b03275e152c74cb32
RasLin1 <rasmus.a.lindstrom@gmail.com>
2025-10-08 20:32:05 +0300

    Started making player functions

--- RasmusMod/creatures.py
+++ RasmusMod/creatures.py
@@ -1,33 +1,41 @@
 from game_functions import get_cordinates
+from db import create_player
+
 
 #Luo pelaajan sanakirjana
-def create_entity(name, airport):
-    return {
+def create_entity(name, airport, type):
+    print(f"DEBUG: create_player() called with name={name}, location={airport["ident"]}")
+    entity = {
         "name": name,
         "country": airport["c_name"],
         "location": airport["ident"],
         "location_name":airport["a_name"],
-        "cordinates": get_cordinates(airport)
+        "cordinates": (airport["lat"], airport["lon"])
     }
+    if type == 1:
+        player = create_player(name, airport["ident"])
+        if player == True:
+            print("Player creation success")
+        else:
+            print("Player creation fail")
+    return entity
 
 def move_entity(entity, airport):
     if airport == False:
         print("Error in moving, possibly invalid icao")
         return {
-        "name": entity["name"],
-        "country": entity["country"],
-        "location": entity["location"],
-        "location_name":entity["location_name"],
-        "cordinates": entity["cordinates"]
+            "name": entity["name"],
+            "country": entity["country"],
+            "location": entity["location"],
+            "location_name":entity["location_name"],
+            "cordinates": entity["cordinates"]
         }
     else:
-        entity["location"] = airport["ident"]
-        entity["cordinates"] = (airport["lat"], airport["lon"])
         return {
             "name": entity["name"],
             "country": airport["c_name"],
             "location": airport["ident"],
             "location_name":airport["a_name"],
-            "cordinates": entity["cordinates"]
+            "cordinates": (airport["lat"], airport["lon"])
         }
     
--- RasmusMod/game.py
+++ RasmusMod/game.py
@@ -6,23 +6,26 @@ allow_game = True
 
 def play():
     round = 1
-    player = creatures.create_entity("Pelaaja", select_random_airport_location())
-    monster = creatures.create_entity("Hirviö", select_random_airport_location())
+    player = creatures.create_entity("Pelaaja", select_random_airport_location(), 1)
+    monster = creatures.create_entity("Hirviö", select_random_airport_location(), 2)
 
     while allow_game:
         distance = game_functions.current_distance(player['cordinates'], monster['cordinates'])
         print(f"{player['name']} located in {player['location_name']} {player['location']} distance to {monster['name']} {distance}km") 
-        airports = game_functions.select_closest_airports(5, player['cordinates'])
-        for x in airports:
-            print(f"Nimi: {x["airport_name"]} | ICAO-koodi: {x["airport_icao"]} | Distance: {x["distance"]}")
-        round_action = input("Kirjoita 'S' jos haluat siirtää paikkaa | Kirjoita 'L' jos haluat levätä ").upper()
-        if round_action == "S":
-            updated_player = game_functions.move(player, input("Anna lentokentän icao-koodi jonne haluat siirtyä: ").upper())
-            player = updated_player
-        elif round_action == "L":
-            print("ZZZ...")
+        if distance == 0.00:
+            print("Same airport as monster")
         else:
-            print("Move invalid input")
+            airports = game_functions.select_closest_airports(5, player['cordinates'])
+            for x in airports:
+                print(f"Nimi: {x["airport_name"]} | ICAO-koodi: {x["airport_icao"]} | Distance: {x["distance"]}")
+            round_action = input("Kirjoita 'S' jos haluat siirtää paikkaa | Kirjoita 'L' jos haluat levätä ").upper()
+            if round_action == "S":
+                updated_player = game_functions.move(player, input("Anna lentokentän icao-koodi jonne haluat siirtyä: ").upper())
+                player = updated_player
+            elif round_action == "L":
+                print("ZZZ...")
+            else:
+                print("Move invalid input")
         round = round + 1
         print(round)
 
