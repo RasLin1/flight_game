e60f105810cbea89aaf6e8755e810d3d7d0e3dc2
Lumikuutti <227412132+lumikuutti@users.noreply.github.com>
2025-10-09 11:14:44 +0300

    update

---
+++ StuMod/start.py
@@ -0,0 +1,9 @@
+print ("Welcome to Legally Distinct Monster Hunter! \n"
+       "You are an agent tasked with capturing monsters that have gotten a bit too wild to be left on their own. \n"
+       "The game is played by typing on the keyboard when prompted. \n"
+       "Your objective is to capture the monsters on the loose, while minimizing the resources you use. \n"
+       "The monsters will move periodically, but to help with this, you can acquire probes to give you clues on how far the monsters are. \n"
+       "You can also acquire other items, such as weapons to help you on your journey.\n"
+       "Better be prepared after all, the monsters will try to resist capture! \n"
+       "However, if you take too long, run out of resources or get taken out, it's game over. \n"
+       "Now let's get started by setting your player handle!")
