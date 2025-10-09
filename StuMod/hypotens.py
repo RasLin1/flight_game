d0dbfb8b670ad830bed95b6feac84ffa1119bd99
Lumikuutti <227412132+lumikuutti@users.noreply.github.com>
2025-10-09 11:32:25 +0300

    update

--- StuMod/hypotens.py
+++ StuMod/hypotens.py
@@ -4,17 +4,27 @@ import math
 def random_hypotenuse():
     side_a = random.randint(1, 25)
     side_b = random.randint(1, 25)
+    attempts = 0
     answer = int(input(
         f"Mikä on hypotenuusan karkea pituus, jos suorakulmaisen kolmion muut sivut ovat {side_a} ja {side_b}?\n"
         f"Anna vastaus kokonaislukuna, pyöristettynä poikkeuksellisesti alaspäin: "))
-    while answer != (math.sqrt(side_a**2 + side_b**2)) // 1:
+    while attempts <= 3 and answer != (math.sqrt(side_a**2 + side_b**2)) // 1:
         answer = int(input("Väärin, yritä uudestaan: "))
-    print("Oikein!")
+        attempts += 1
+    if attempts > 3 and answer != (math.sqrt(side_a**2 + side_b**2)) // 1:
+        print ("Valitettavasti en voi sinua nyt palkita. Parempi onni seuraavan kysymyksen kanssa.")
+    else:
+        sql = "SELECT event_reward_type, event_reward_value FROM events WHERE event_description = 'hypotens';"
+        # print(sql)
+        cursor = connection.cursor(dictionary=True)
+        cursor.execute(sql)
+        result = cursor.fetchall()
+        print(f"Oikein! Palkinnoksi saat {result["event_reward_value"]} {result["event_reward_type"]}")
     return
 
-command = input("Lopeta painamalle enter, jatka antamalla mikä tahansa stringgi: ")
-while command != "":
+#command = input("Lopeta painamalle enter, jatka antamalla mikä tahansa stringgi: ")
+#while command != "":
     random_hypotenuse()
     command = input("Lopeta painamalle enter, jatka antamalla mikä tahansa stringgi: ")
 
-print("Kiitos käytöstä.")
+#print("Kiitos käytöstä.")
