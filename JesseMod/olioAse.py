79faa2eea57982cba6f86cbdfc62ab3dd9fa8725
RasLin1 <117277514+RasLin1@users.noreply.github.com>
2025-10-10 08:41:39 +0300

    Delete JesseMod directory
    Deleting the individual directories. Final contents are combination of the individual ones.

--- JesseMod/olioAse.py
+++
@@ -1,6 +0,0 @@
-def olioLisays(alienId,alienName) :
-    healthValue = float(input("Anna alieenin health arvo :"))
-    damageValue = float(input("Anna alieenin damage arvo :"))
-
-    return  alienId,alienName,healthValue,damageValue
-newAlien = olioLisays(1,"Scytheworm")
